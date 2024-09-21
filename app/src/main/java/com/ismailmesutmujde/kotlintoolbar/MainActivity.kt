3f57236d5e5d14fcf3f3b3e80011c0e569a2d957
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 13:47:50 +0300

    updated: toolbar

--- app/src/main/java/com/ismailmesutmujde/kotlintoolbar/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlintoolbar/MainActivity.kt
@@ -2,13 +2,23 @@ package com.ismailmesutmujde.kotlintoolbar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlintoolbar.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var bindingMain : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
 
-        setContentView(R.layout.activity_main)
+        bindingMain.toolbar.title = "Custom Toolbar Title"
+        bindingMain.toolbar.subtitle = "Custom Toolbar SubTitle"
+        bindingMain.toolbar.setLogo(R.drawable.image)
+        bindingMain.toolbar.setTitleTextColor(resources.getColor(R.color.white))
 
     }
 }
